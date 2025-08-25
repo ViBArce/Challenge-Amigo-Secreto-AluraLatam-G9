ac9d12507c969ec7a0984f39da1070fe5733ecbe
ViBArce <barrientos.arce@gmail.com>
2025-08-24 22:16:23 -0600

    Se axadio la captura del input, la validacion de repeticion y campo vacio en array listaAmigos condicionando el push, se agrego la funcion de limpiar campo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigos(){
+    let capturaNombres = document.getElementById('amigo').value;
+    if (capturaNombres == '') {
+        alert('Por favor ingresa un nombre');
+    }else if (listaAmigos.includes(capturaNombres)){
+        alert('Tu amigo ya esta jugando, por favor ingresa otro nombre');    
+    }else{
+        listaAmigos.push(capturaNombres);
+    }
+    limpiarCaja();
+    console.log(listaAmigos);
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = ''; 
+} 
