7e312c5e1fc4c57f3c6f9dfe9d53150cd50eafcf
ViBArce <barrientos.arce@gmail.com>
2025-08-24 22:47:09 -0600

    Se agrego obtener elemento de la lista, limpiar lista, bocle de confirmacion de array y agregar informacion a la lista. En index se cambio el id de ul.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let listaAmigos = [];
 
 function agregarAmigos(){
     let capturaNombres = document.getElementById('amigo').value;
+    let lista = document.getElementById('amigosJugando');
     if (capturaNombres == '') {
         alert('Por favor ingresa un nombre');
     }else if (listaAmigos.includes(capturaNombres)){
@@ -11,9 +12,14 @@ function agregarAmigos(){
         listaAmigos.push(capturaNombres);
     }
     limpiarCaja();
-    console.log(listaAmigos);
+    lista.innerHTML = '';
+    for(let i = 0; i < listaAmigos.length; i++){
+        let li = document.createElement('li');
+        li.innerText = listaAmigos[i];
+        lista.appendChild(li);
+    }
 }
 
 function limpiarCaja(){
-    document.querySelector('#amigo').value = ''; 
+    document.querySelector('#amigo').value = '';
 } 
